d3a30301d10082ae10cd18e46a59efe8d2664679
Jennings Zhang <jenni_zh@protonmail.com>
2021-02-09 23:19:49 -0500

    Fix swarm block

--- provision/swarm.sh
+++ provision/swarm.sh
@@ -24,37 +24,19 @@ else
 fi
 
 function end_swarm () {
-  $@
+  echo Goodbye
+  $leave_command
   exit_status=$?
   STOP=y
   exit $exit_status
 }
 
-trap "end_swarm $leave_command" TERM INT
+trap end_swarm TERM INT
 
 # Now, we just want to sleep and let the script end via the
 # function end_swarm which is invoked asynchronously by
 #
 #     docker-compose down
-#
-# The specific solution below is pedantic.
-#
-# 1. `docker wait` blocks without polling, which is something bash cannot do.
-# 2. poll until execution of `end_swarm` is finished
-# 3. script should exit via `exit` command in `end_swarm`
-# 4. the last lines `sleep 1 && exit 1` should never occur,
-#    but are there in case a solar neutrino flips a bit in memory
-#    (i.e. just to have complete coverage in our execution tree)
-#
-# Everything would work the same way if we just did
-#
-#     while true; do sleep 10000; done
 
-docker wait pman
-
-until [ "$STOP" = "y" ]; do
-  sleep 1
-done
-
-sleep 1
-exit 1
+docker wait swarm-status &
+wait
