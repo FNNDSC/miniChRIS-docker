a38083e427c18f45a18dfa8817ed9d25cf9d7039
Jennings Zhang <jenni_zh@protonmail.com>
2021-02-22 21:52:38 -0500

    Migrate to new ChRIS backend

--- provision/setup.sh
+++ provision/setup.sh
@@ -10,6 +10,14 @@ if [ -f .setup ]; then
   echo "Already set up"
 fi
 
+
+docker exec chris python manage.py shell -c '
+from core.models import ChrisInstance
+inst = ChrisInstance.load()
+inst.job_id_prefix = "chris-minimake-jid-"
+inst.save()'
+
+
 superuser_script='
 from django.contrib.auth.models import User
 User.objects.create_superuser(username="chris", password="chris1234", email="dev@babymri.org")'
@@ -18,9 +26,11 @@ docker exec chris python manage.py shell -c "$superuser_script"
 docker exec chris_store python manage.py shell -c "$superuser_script"
 
 docker exec chris python plugins/services/manager.py \
-  add host "http://pfcon.local:5005" --description "Local compute"
+  add host "http://pfcon.local:5005/api/v1/" --description "Local compute"
 
 # pl-dircopy
-docker exec chris python plugins/services/manager.py register host --pluginurl https://chrisstore.co/api/v1/plugins/7/
+docker exec chris python plugins/services/manager.py register host --pluginurl \
+  https://chrisstore.co/api/v1/plugins/25/
+docker pull fnndsc/pl-dircopy:2.1.0
 
 touch .setup
