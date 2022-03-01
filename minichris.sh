e080b6d021ccae2b4566ee6f4180c8ec1392b4e4
Jennings Zhang <jenni_zh@protonmail.com>
2022-02-28 19:20:10 -0500

    Fix negation

--- minichris.sh
+++ minichris.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(docker info -f '{{ .Swarm.LocalNodeState }}')" = "active" ] && docker inspect swarm-status > /dev/null 2>&1; then
+if [ "$(docker info -f '{{ .Swarm.LocalNodeState }}')" = "active" ] && ! docker inspect swarm-status > /dev/null 2>&1; then
   echo "WARNING: docker swarm is currently active. Proceed? [yN]"
   read -n 1 proceed
   if [ $proceed != 'y' ]; then
