d0dde9f15c7aee45e72276c956b06958ce3180da
Jennings Zhang <jenni_zh@protonmail.com>
2023-03-17 04:34:48 -0400

    Goodbye swarm!

--- minichris.sh
+++ minichris.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 
-if [ "$(docker info -f '{{ .Swarm.LocalNodeState }}')" = "active" ] && ! docker inspect swarm-status > /dev/null 2>&1; then
-  echo "WARNING: docker swarm is currently active. Proceed? [yN]"
-  read -n 1 proceed
-  if [ $proceed != 'y' ]; then
-    exit
-  fi
-  echo "Reset swarm? Unsaved data will be lost! [yN]"
-  read -n 1 reset_swarm
-  if [ $reset_swarm = 'y' ]; then
-    set -ex
-    docker swarm leave --force
-    { set +x; } 2> /dev/null
-  else
-    echo "WARNING: swarm state is stale, plugin instances might fail!"
-  fi
-fi
-
 if [ "$CI" = "true" ]; then
-  not='-T'
+  notty='-T'
 fi
 
 # change to directory where this script lives
-cd $(dirname "$0")
+cd "$(dirname "$(readlink -f "$0")")"
 
 set -ex
 docker compose up -d
-exec docker compose run --rm $not chrisomatic
+exec docker compose run --rm $notty chrisomatic
--- unmake.sh
+++ unmake.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # change to directory where this script lives
-cd $(dirname "$0")
+cd "$(dirname "$(readlink -f "$0")")"
+
+set -ex
+
+# remove all plugin instance jobs
+pls=$(docker ps -q -f 'label=org.chrisproject.miniChRIS=plugininstance')
+[ -z "$pls" ] || docker rm -fv $pls
 
 # stop and remove everything
 docker compose down -v
