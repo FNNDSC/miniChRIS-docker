3db889c9e8c2ec3f5490273bb5e412a27d198310
Jennings Zhang <jenni_zh@protonmail.com>
2024-03-25 15:53:13 -0400

    Switch curl flag --data-binary to -T
    Fixes OOM when file is large

--- scripts/upload2orthanc.sh
+++ scripts/upload2orthanc.sh
@@ -9,4 +9,4 @@ fi
 url="http://localhost:8042/instances"
 
 find -L "$1" -type f -name '*.dcm' \
-  | parallel --bar -j 4 "curl -sfX POST -u orthanc:orthanc http://localhost:8042/instances -H Expect: -H 'Content-Type: application/dicom' --data-binary @'{}' -o /dev/null"
+  | parallel --bar -j 4 "curl -sSfX POST -u orthanc:orthanc http://localhost:8042/instances -H Expect: -H 'Content-Type: application/dicom' -T {} -o /dev/null"
